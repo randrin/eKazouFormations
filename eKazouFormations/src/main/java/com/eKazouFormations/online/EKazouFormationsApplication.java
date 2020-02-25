a4837d353db734d8a955faa9b0c99c2f30db3851
@nzeukangnimpar <nzeukangrandrin@gmail.com>
2020-02-25 11:05:56 +0100

    Set Up projet

---
+++ eKazouFormations/src/main/java/com/eKazouFormations/online/EKazouFormationsApplication.java
@@ -0,0 +1,13 @@
+package com.eKazouFormations.online;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EKazouFormationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EKazouFormationsApplication.class, args);
+	}
+
+}
